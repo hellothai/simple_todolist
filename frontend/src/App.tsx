8b60799a75426bc7c5512a878c783d75c1aa6c0a
Thais Farias <1171228@isep.ipp.pt>
2021-07-23 16:35:38 +0100

    Refs #1 added an initial structure frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Register from './components/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="App">      
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          OI :D
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>
+      <Register></Register>
+      </button>
     </div>
   );
 }
---
+++ frontend/src/components/Register.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Register() {
+  return (
+    <div className="Register">
+        <p>
+         Sign Up 
+        </p>
+    </div>
+  );
+}
+
+export default Register;
