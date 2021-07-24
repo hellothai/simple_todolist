a1fae45eb1278e0de4f7cab8759a38ef4ceadbdd
Breno Pacheco <brenoleonhardt@gmail.com>
2021-07-24 15:06:05 +0100

    feat(setup) closes #5 docker-compose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log('wow');
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get( "/", ( _req, res ): void => {
+    res.send( "Hello world!" );
+} );
+
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
