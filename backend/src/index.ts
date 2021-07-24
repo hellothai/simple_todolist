aec97503df9c2519bcc61d2e6bf8726db0aff827
Breno Pacheco <brenoleonhardt@gmail.com>
2021-07-24 18:00:48 +0100

    feat(auth): refs #12 and #9

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,47 @@
-import express from "express";
+import jwt from 'jsonwebtoken';
+import express, { Request } from 'express';
+import cors from 'cors';
+
 const app = express();
 const port = 3000;
+const secret = 'xlI8TySj5eX72D9imPMEPW6RavRIeIfbU6zNmu+AFUyYLrziFJzyvVVjFYnSfJXNcGVVtAyct2EmhWfTlCS7Ow==';
 
-app.get( "/", ( _req, res ): void => {
-    res.send( "Hello world!" );
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.get( '/', ( _req, res ): void => {
+  res.status(200).json({message: 'Hello world!'});
 } );
 
+// user / password
+const users = new Map<string, string>();
+users.set('thais', '123456');
+users.set('breno', '123456');
+
+type LoginRequest = {
+  name: string,
+  password: string
+}
+
+app.post('/login', ( req: Request<LoginRequest>, res ): void => {
+  console.log('>>> here');
+  console.log('>> ', req.body);
+  const { name, password } = req.body;
+  console.log(users.get(name));
+  if (users.has(name) && users.get(name) === password) {
+    const token = jwt.sign({
+      name: name,
+      role: 'user'
+    }, secret);
+    res.status(200).json({token});
+  } else {
+    res.status(400).send(
+      'Authentication failed.'
+    );
+  }
+});
+
 app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
+  console.log( `server started at http://localhost:${ port }` );
 } );
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -2,19 +2,85 @@ import React from 'react';
 import '../App.css';
 import Card from '@material-ui/core/Card';
 
+type LoginForm = {
+  name: string;
+  password: string;
+};
+
+async function login(data: LoginForm) {
+  console.log('data: ', data);
+  const response = await fetch('http://localhost:9001/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  if (response.status === 200) {
+    try {
+      const { token } = await response.json();
+      return token;
+    } catch (err) {
+      throw new Error(`failed: ${err.message}`);
+    }
+  } else {
+    const error = await response.text();
+    throw new Error(`failed: ${error}`);
+  }
+}
+
 function Login() {
+  const [name, setName] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+  const [error, setError] = React.useState<string>('');
+  const [success, setSuccess] = React.useState<string>('');
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (name.length < 3 || password.length < 3) {
+      setError('Invalid input');
+      return;
+    } else {
+      setError('');
+    }
+    login({
+      name,
+      password,
+    })
+      .then((token) => {
+        setSuccess(`Logged in successfully as ${name}:\nToken: ${token}`);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
   return (
-    <Card elevation={2} className="login" style={{maxHeight: 800}}>
-        <p>
-         name:
-         <input></input>
-        </p>
-        
-        <p>
-         password:
-         <input></input>
-        </p>
-       
+    <Card elevation={2} className="login" style={{ maxHeight: 800 }}>
+      {success.length > 0 ? (
+        <div>{success}</div>
+      ) : (
+        <form onSubmit={(e) => submit(e)}>
+          <p>
+            name:
+            <input
+              type="text"
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </p>
+          <p>
+            password:
+            <input
+              type="text"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </p>
+          <input type="submit" value="Submit" />
+          {error.length > 0 ? <div>{error}</div> : null}
+        </form>
+      )}
     </Card>
   );
 }
